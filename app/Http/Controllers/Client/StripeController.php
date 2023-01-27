58ce19c8ce7f9bb92c5fb01d8b8d05de18015b77
Piotr N <piotr_naw99@outlook.com>
2023-01-27 20:07:56 +0100

    Mail and payment part 1 27012023

--- app/Http/Controllers/Client/StripeController.php
+++ app/Http/Controllers/Client/StripeController.php
@@ -5,23 +5,107 @@ namespace App\Http\Controllers\Client;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Gloudemans\Shoppingcart\Facades\Cart;
+use App\Models\Order;
+use App\Models\OrderItem;
+use Illuminate\Support\Facades\Session;
+
+use Auth;
+use Carbon\Carbon;
+
+use Illuminate\Support\Facades\Mail;
+use App\Mail\OrderMail;
+
 class StripeController extends Controller
 {
     public function OrderStripe(Request $request){
 
       \Stripe\Stripe::setApiKey('sk_test_51MUboxIRJ8ix16XZzDJ9a4YmMxrhYmwhut1HJ3PfnrPNKcqFImx6sIryuA9gV61RYFkIBz4mfBYf7PizT95NN11K00Yfg09OGw');
 
+      if (Session::has('coupon')) {
+    		$total_amount = Session::get('coupon')['total_amount'];
+    	}else{
+    		$total_amount = round(Cart::total());
+    	}
 
       $token = $_POST['stripeToken'];
       $charge = \Stripe\Charge::create([
-      'amount' => 999*100,
+      'amount' => $total_amount*100,
       'currency' => 'PLN',
       'description' => 'Sweet Dream',
       'source' => $token,
-      'metadata' => ['order_id' => '6735'],
+      'metadata' => ['order_id' => uniqid()],
         ]);
 
-        dd($charge);
+        $order_id = Order::insertGetId([
+          'user_id' => Auth::id(),
+          'area_id' => $request->area_id,
+          'district_id' => $request->district_id,
+          'state_id' => $request->state_id,
+          'name' => $request->name,
+          'email' => $request->email,
+          'phone' => $request->phone,
+          'post_code' => $request->post_code,
+          'notes' => $request->notes,
+   
+          'payment_type' => 'Stripe',
+          'payment_method' => 'Stripe',
+          'payment_type' => $charge->payment_method,
+          'transaction_id' => $charge->balance_transaction,
+          'currency' => $charge->currency,
+          'amount' => $total_amount,
+          'order_number' => $charge->metadata->order_id,
+   
+          'invoice_no' => 'EOS'.mt_rand(10000000,99999999),
+          'order_date' => Carbon::now()->format('d F Y'),
+          'order_month' => Carbon::now()->format('F'),
+          'order_year' => Carbon::now()->format('Y'),
+          'status' => 'Pending',
+          'created_at' => Carbon::now(),	 
+   
+        ]);
+
+          $invoice = Order::findOrFail($order_id);
+          $data = [
+            'invoice_no' => $invoice->invoice_no,
+            'amount' => $total_amount,
+            'name' => $invoice->name,
+              'email' => $invoice->email,
+          ];
+  
+          Mail::to($request->email)->send(new OrderMail($data));
+
+          $carts = Cart::content();
+          foreach ($carts as $cart) {
+            OrderItem::insert([
+              'order_id' => $order_id, 
+              'product_id' => $cart->id,
+              'weight' => $cart->options->weight,
+              'qty' => $cart->qty,
+              'price' => $cart->price,
+              'created_at' => Carbon::now(),
+
+            ]);
+          }
+
+
+          if (Session::has('coupon')) {
+            Session::forget('coupon');
+          }
+
+          Cart::destroy();
+
+          $notification = array(
+            'message' => 'Your order place successfully / Twoje zamówienie zostało pomyślnie złożone',
+            'alert-type' => 'success'
+          );
+
+          return redirect()->route('dashboard')->with($notification);
+
+
+          } 
+
+        // dd($charge);
 
     }
-}
+
--- resources/views/frontend/payment/stripe.blade.php
+++ resources/views/frontend/payment/stripe.blade.php
@@ -95,7 +95,14 @@ background-color: #fefde5 !important;}
           @csrf
           <div class="form-row">
           <label for="card-element">
-          {{ __('Credit or debit card') }}
+            <input type="hidden" name="name" value="{{ $data['shipping_name'] }}">
+            <input type="hidden" name="email" value="{{ $data['shipping_email'] }}">
+            <input type="hidden" name="phone" value="{{ $data['shipping_phone'] }}">
+            <input type="hidden" name="post_code" value="{{ $data['post_code'] }}">
+            <input type="hidden" name="area_id" value="{{ $data['area_id'] }}">
+            <input type="hidden" name="district_id" value="{{ $data['district_id'] }}">
+            <input type="hidden" name="state_id" value="{{ $data['state_id'] }}">
+            <input type="hidden" name="notes" value="{{ $data['notes'] }}">           
           </label>
 
           <div id="card-element">
@@ -144,9 +151,7 @@ invalid: {
 };
 // Create an instance of the card Element.
 var card = elements.create('card', {style: style});
-// Add an instance of the card Element into the `card-element` <div>.
 card.mount('#card-element');
-// Handle real-time validation errors from the card Element.
 card.on('change', function(event) {
 var displayError = document.getElementById('card-errors');
 if (event.error) {
@@ -155,31 +160,25 @@ if (event.error) {
   displayError.textContent = '';
 }
 });
-// Handle form submission.
 var form = document.getElementById('payment-form');
 form.addEventListener('submit', function(event) {
 event.preventDefault();
 stripe.createToken(card).then(function(result) {
   if (result.error) {
-    // Inform the user if there was an error.
     var errorElement = document.getElementById('card-errors');
     errorElement.textContent = result.error.message;
   } else {
-    // Send the token to your server.
     stripeTokenHandler(result.token);
   }
 });
 });
-// Submit the form with the token ID.
 function stripeTokenHandler(token) {
-// Insert the token ID into the form so it gets submitted to the server
 var form = document.getElementById('payment-form');
 var hiddenInput = document.createElement('input');
 hiddenInput.setAttribute('type', 'hidden');
 hiddenInput.setAttribute('name', 'stripeToken');
 hiddenInput.setAttribute('value', token.id);
 form.appendChild(hiddenInput);
-// Submit the form
 form.submit();
 }
 </script>
